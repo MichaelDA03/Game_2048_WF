48ca6e496a84a6d7ab4f17fe4c751e31e32c2c8f
MichaelDA03 <michdaq@gmail.com>
2023-12-18 15:00:10 +0100

    Mise en place de la grille

--- Game_2048_WF/Form1.Designer.cs
+++ Game_2048_WF/Form1.Designer.cs
@@ -28,20 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl2048 = new System.Windows.Forms.Label();
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.pnl4x4 = new System.Windows.Forms.Panel();
+            this.pnl4x3 = new System.Windows.Forms.Panel();
+            this.pnl4x2 = new System.Windows.Forms.Panel();
+            this.pnl4x1 = new System.Windows.Forms.Panel();
+            this.pnl3x4 = new System.Windows.Forms.Panel();
+            this.pnl3x3 = new System.Windows.Forms.Panel();
+            this.pnl3x2 = new System.Windows.Forms.Panel();
+            this.pnl3x1 = new System.Windows.Forms.Panel();
+            this.pnl2x4 = new System.Windows.Forms.Panel();
+            this.pnl2x3 = new System.Windows.Forms.Panel();
+            this.pnl2x2 = new System.Windows.Forms.Panel();
+            this.pnl2x1 = new System.Windows.Forms.Panel();
+            this.pnl1x4 = new System.Windows.Forms.Panel();
+            this.pnl1x3 = new System.Windows.Forms.Panel();
+            this.pnl1x2 = new System.Windows.Forms.Panel();
+            this.pnl1x1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblNumScore = new System.Windows.Forms.Label();
+            this.pnlGrid.SuspendLayout();
+            this.pnl1x1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lbl2048
+            // 
+            this.lbl2048.AutoSize = true;
+            this.lbl2048.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2048.Location = new System.Drawing.Point(43, 50);
+            this.lbl2048.Name = "lbl2048";
+            this.lbl2048.Size = new System.Drawing.Size(93, 39);
+            this.lbl2048.TabIndex = 1;
+            this.lbl2048.Text = "2048";
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlGrid.Controls.Add(this.pnl4x4);
+            this.pnlGrid.Controls.Add(this.pnl4x3);
+            this.pnlGrid.Controls.Add(this.pnl4x2);
+            this.pnlGrid.Controls.Add(this.pnl4x1);
+            this.pnlGrid.Controls.Add(this.pnl3x4);
+            this.pnlGrid.Controls.Add(this.pnl3x3);
+            this.pnlGrid.Controls.Add(this.pnl3x2);
+            this.pnlGrid.Controls.Add(this.pnl3x1);
+            this.pnlGrid.Controls.Add(this.pnl2x4);
+            this.pnlGrid.Controls.Add(this.pnl2x3);
+            this.pnlGrid.Controls.Add(this.pnl2x2);
+            this.pnlGrid.Controls.Add(this.pnl2x1);
+            this.pnlGrid.Controls.Add(this.pnl1x4);
+            this.pnlGrid.Controls.Add(this.pnl1x3);
+            this.pnlGrid.Controls.Add(this.pnl1x2);
+            this.pnlGrid.Controls.Add(this.pnl1x1);
+            this.pnlGrid.Location = new System.Drawing.Point(50, 92);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(425, 425);
+            this.pnlGrid.TabIndex = 2;
+            // 
+            // pnl4x4
+            // 
+            this.pnl4x4.Location = new System.Drawing.Point(320, 320);
+            this.pnl4x4.Name = "pnl4x4";
+            this.pnl4x4.Size = new System.Drawing.Size(100, 100);
+            this.pnl4x4.TabIndex = 15;
+            // 
+            // pnl4x3
+            // 
+            this.pnl4x3.Location = new System.Drawing.Point(215, 320);
+            this.pnl4x3.Name = "pnl4x3";
+            this.pnl4x3.Size = new System.Drawing.Size(100, 100);
+            this.pnl4x3.TabIndex = 14;
+            // 
+            // pnl4x2
+            // 
+            this.pnl4x2.Location = new System.Drawing.Point(110, 320);
+            this.pnl4x2.Name = "pnl4x2";
+            this.pnl4x2.Size = new System.Drawing.Size(100, 100);
+            this.pnl4x2.TabIndex = 13;
+            // 
+            // pnl4x1
+            // 
+            this.pnl4x1.Location = new System.Drawing.Point(5, 320);
+            this.pnl4x1.Name = "pnl4x1";
+            this.pnl4x1.Size = new System.Drawing.Size(100, 100);
+            this.pnl4x1.TabIndex = 12;
+            // 
+            // pnl3x4
+            // 
+            this.pnl3x4.Location = new System.Drawing.Point(320, 215);
+            this.pnl3x4.Name = "pnl3x4";
+            this.pnl3x4.Size = new System.Drawing.Size(100, 100);
+            this.pnl3x4.TabIndex = 11;
+            // 
+            // pnl3x3
+            // 
+            this.pnl3x3.Location = new System.Drawing.Point(215, 215);
+            this.pnl3x3.Name = "pnl3x3";
+            this.pnl3x3.Size = new System.Drawing.Size(100, 100);
+            this.pnl3x3.TabIndex = 10;
+            // 
+            // pnl3x2
+            // 
+            this.pnl3x2.Location = new System.Drawing.Point(110, 215);
+            this.pnl3x2.Name = "pnl3x2";
+            this.pnl3x2.Size = new System.Drawing.Size(100, 100);
+            this.pnl3x2.TabIndex = 9;
+            // 
+            // pnl3x1
+            // 
+            this.pnl3x1.Location = new System.Drawing.Point(5, 215);
+            this.pnl3x1.Name = "pnl3x1";
+            this.pnl3x1.Size = new System.Drawing.Size(100, 100);
+            this.pnl3x1.TabIndex = 8;
+            // 
+            // pnl2x4
+            // 
+            this.pnl2x4.Location = new System.Drawing.Point(320, 110);
+            this.pnl2x4.Name = "pnl2x4";
+            this.pnl2x4.Size = new System.Drawing.Size(100, 100);
+            this.pnl2x4.TabIndex = 7;
+            // 
+            // pnl2x3
+            // 
+            this.pnl2x3.Location = new System.Drawing.Point(215, 110);
+            this.pnl2x3.Name = "pnl2x3";
+            this.pnl2x3.Size = new System.Drawing.Size(100, 100);
+            this.pnl2x3.TabIndex = 6;
+            // 
+            // pnl2x2
+            // 
+            this.pnl2x2.Location = new System.Drawing.Point(110, 110);
+            this.pnl2x2.Name = "pnl2x2";
+            this.pnl2x2.Size = new System.Drawing.Size(100, 100);
+            this.pnl2x2.TabIndex = 5;
+            // 
+            // pnl2x1
+            // 
+            this.pnl2x1.Location = new System.Drawing.Point(5, 110);
+            this.pnl2x1.Name = "pnl2x1";
+            this.pnl2x1.Size = new System.Drawing.Size(100, 100);
+            this.pnl2x1.TabIndex = 4;
+            // 
+            // pnl1x4
+            // 
+            this.pnl1x4.Location = new System.Drawing.Point(320, 5);
+            this.pnl1x4.Name = "pnl1x4";
+            this.pnl1x4.Size = new System.Drawing.Size(100, 100);
+            this.pnl1x4.TabIndex = 3;
+            // 
+            // pnl1x3
+            // 
+            this.pnl1x3.Location = new System.Drawing.Point(215, 5);
+            this.pnl1x3.Name = "pnl1x3";
+            this.pnl1x3.Size = new System.Drawing.Size(100, 100);
+            this.pnl1x3.TabIndex = 2;
+            // 
+            // pnl1x2
+            // 
+            this.pnl1x2.Location = new System.Drawing.Point(110, 5);
+            this.pnl1x2.Name = "pnl1x2";
+            this.pnl1x2.Size = new System.Drawing.Size(100, 100);
+            this.pnl1x2.TabIndex = 1;
+            // 
+            // pnl1x1
+            // 
+            this.pnl1x1.Controls.Add(this.label1);
+            this.pnl1x1.Location = new System.Drawing.Point(5, 5);
+            this.pnl1x1.Name = "pnl1x1";
+            this.pnl1x1.Size = new System.Drawing.Size(100, 100);
+            this.pnl1x1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(391, 50);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(64, 13);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "Votre score:";
+            // 
+            // lblNumScore
+            // 
+            this.lblNumScore.AutoSize = true;
+            this.lblNumScore.Location = new System.Drawing.Point(414, 70);
+            this.lblNumScore.Name = "lblNumScore";
+            this.lblNumScore.Size = new System.Drawing.Size(13, 13);
+            this.lblNumScore.TabIndex = 4;
+            this.lblNumScore.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 577);
+            this.ClientSize = new System.Drawing.Size(766, 656);
+            this.Controls.Add(this.lblNumScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.lbl2048);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.pnlGrid.ResumeLayout(false);
+            this.pnl1x1.ResumeLayout(false);
+            this.pnl1x1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label lbl2048;
+        private System.Windows.Forms.Panel pnlGrid;
+        private System.Windows.Forms.Panel pnl4x4;
+        private System.Windows.Forms.Panel pnl4x3;
+        private System.Windows.Forms.Panel pnl4x2;
+        private System.Windows.Forms.Panel pnl4x1;
+        private System.Windows.Forms.Panel pnl3x4;
+        private System.Windows.Forms.Panel pnl3x3;
+        private System.Windows.Forms.Panel pnl3x2;
+        private System.Windows.Forms.Panel pnl3x1;
+        private System.Windows.Forms.Panel pnl2x4;
+        private System.Windows.Forms.Panel pnl2x3;
+        private System.Windows.Forms.Panel pnl2x2;
+        private System.Windows.Forms.Panel pnl2x1;
+        private System.Windows.Forms.Panel pnl1x4;
+        private System.Windows.Forms.Panel pnl1x3;
+        private System.Windows.Forms.Panel pnl1x2;
+        private System.Windows.Forms.Panel pnl1x1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblNumScore;
+        private System.Windows.Forms.Label label1;
     }
 }
 
